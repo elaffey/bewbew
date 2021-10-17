212dfd8cb3bedc5f574d0537801620a9a07b34f9
Eamonn Laffey <eamonn.laffey@gmail.com>
2021-10-17 19:47:15 +1100

    keypair_maker

--- server_sdk/src/lib.rs
+++ server_sdk/src/lib.rs
@@ -1,9 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use lazy_static::lazy_static;
+use ring::signature::Ed25519KeyPair;
+
+lazy_static! {
+    static ref KEY_PAIR: Ed25519KeyPair = load_key_pair();
+}
+
+fn load_key_pair() -> Ed25519KeyPair {
+    use std::io::Read;
+    let mut file = std::fs::File::open("hi.p8").unwrap();
+    let mut contents: Vec<u8> = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+    Ed25519KeyPair::from_pkcs8(&contents).unwrap()
 }
 
 pub fn hi() -> u32 {
