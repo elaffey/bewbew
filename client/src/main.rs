4822e83905c0f661f4ee83212b26e59ad29a0339
Eamonn Laffey <eamonn.laffey@anz.com>
2021-10-30 21:47:03 +1100

    sign-up + login APIs working

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,12 @@
+#[allow(dead_code)]
 use error::Error;
-use types::{PlusOneResType, Req};
+use reqwest::Client;
+use types::{LoginReq, LoginRes, PlusOneRes, Req, SignUpReq};
 
-async fn call_apis() -> Result<(), Error> {
-    let client = reqwest::Client::new();
-    let req = Req::PlusOne(4);
+async fn call<T>(client: &Client, req: Req) -> Result<T, Error>
+where
+    T: serde::de::DeserializeOwned,
+{
     let req_bytes = types::ser(&req).unwrap();
     let res = client
         .post("http://localhost:8080")
@@ -11,29 +14,57 @@ async fn call_apis() -> Result<(), Error> {
         .send()
         .await
         .map_err(|e| Error::wrap("calling api", e))?;
-    let code = res.status();
-    dbg!(code);
     let bytes = res
         .bytes()
         .await
         .map_err(|e| Error::wrap("getting request bytes", e))?;
-    let added: PlusOneResType = types::de(&bytes).unwrap();
-    match added {
-        Ok(a) => {
-            dbg!(a);
-        }
-        Err(e) => {
-            dbg!(e);
-        }
+    types::de::<T>(&bytes)
+}
+
+async fn do_adding(client: &Client, n: u32) -> Result<Result<PlusOneRes, Error>, Error> {
+    let req = Req::PlusOne(n);
+    call(&client, req).await
+}
+
+async fn sign_up(client: &Client, req: SignUpReq) -> Result<Result<(), Error>, Error> {
+    let req = Req::SignUpReq(req);
+    call(&client, req).await
+}
+
+async fn login(client: &Client, req: LoginReq) -> Result<Result<LoginRes, Error>, Error> {
+    let req = Req::Login(req);
+    call(&client, req).await
+}
+
+async fn test_do_add(client: &Client) {
+    let req = 3;
+    let res = do_adding(&client, req).await;
+    println!("{:?}", res);
+}
+
+async fn test_sign_up(client: &Client) {
+    let req = SignUpReq {
+        username: String::from("eamonn"),
+        password: String::from("secret"),
     };
-    Ok(())
+    let res = sign_up(&client, req).await;
+    println!("{:?}", res);
+}
+
+async fn test_login(client: &Client) {
+    let req = LoginReq {
+        username: String::from("eamonn"),
+        password: String::from("secret"),
+    };
+    let res = login(&client, req).await;
+    println!("{:?}", res);
 }
 
 async fn go() {
-    match call_apis().await {
-        Ok(_) => println!("success"),
-        Err(e) => eprintln!("error - {}", e),
-    }
+    let client = reqwest::Client::new();
+    test_do_add(&client).await;
+    test_sign_up(&client).await;
+    test_login(&client).await;
 }
 
 fn main() {
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 use error::Error;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, StatusCode};
+use lazy_static::lazy_static;
+use sdk::state::State;
 use std::convert::Infallible;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tracing::metadata::LevelFilter;
@@ -8,6 +10,10 @@ use tracing::{error, info, span, Level};
 use tracing_futures::Instrument;
 use tracing_subscriber::filter::EnvFilter;
 use types::{PlusOneRes, PlusOneResType, Req};
+
+lazy_static! {
+    static ref STATE: State = State::default().unwrap();
+}
 
 async fn read_request(req: Request<Body>) -> Result<impl std::io::Read, Error> {
     use hyper::body::Buf;
@@ -28,34 +34,50 @@ fn do_adding(n: u32) -> PlusOneResType {
     Ok(res)
 }
 
+async fn route(req: Request<Body>) -> Result<Response<Body>, Error> {
+    let span = span!(Level::TRACE, "route",);
+    let _enter = span.enter();
+
+    let reader = read_request(req).await?;
+    let req = types::de_from(reader)?;
+    match req {
+        Req::Login(r) => {
+            info!("login");
+            let res = sdk::apis::login(&STATE, r);
+            let bytes = types::ser(&res)?;
+            Ok(Response::new(Body::from(bytes)))
+        }
+        Req::SignUpReq(r) => {
+            info!("sign up");
+            let res = sdk::apis::sign_up(&STATE, r);
+            let bytes = types::ser(&res)?;
+            Ok(Response::new(Body::from(bytes)))
+        }
+        Req::PlusOne(r) => {
+            info!("plus one");
+            let res = do_adding(r);
+            let bytes = types::ser(&res)?;
+            Ok(Response::new(Body::from(bytes)))
+        }
+    }
+}
+
 async fn serve(req: Request<Body>) -> Response<Body> {
     let span = span!(
         Level::TRACE,
-        "request",
+        "serve",
         method = ?req.method(),
         uri = ?req.uri(),
         headers = ?req.headers()
     );
     let _enter = span.enter();
     info!("received request");
-    let reader = read_request(req).await.unwrap();
-    let derd = types::de_from(reader);
-    match derd {
-        Ok(r) => {
-            dbg!(&r);
-            match r {
-                Req::PlusOne(n) => {
-                    let res = do_adding(n);
-                    let bytes = types::ser(&res).unwrap();
-                    Response::new(Body::from(bytes))
-                }
-                _ => Response::new(Body::from("hihi")),
-            }
-        }
+    match route(req).await {
+        Ok(res) => res,
         Err(e) => {
             let err_msg = format!("Error - {}", e);
             Response::builder()
-                .status(StatusCode::BAD_REQUEST)
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
                 .body(Body::from(err_msg))
                 .unwrap()
         }
